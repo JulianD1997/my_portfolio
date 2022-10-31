78860e668400af483510a6c208aef9d6bbd3d586
Julian Casallas <julian.casallasb@gmail.com>
2022-10-31 00:55:18 -0300

    carrusel proyectos

--- projects/stactic/js/portfolio.js
+++ projects/stactic/js/portfolio.js
@@ -1,3 +1,4 @@
+//fullpage
 const pageSection = new fullpage('#fullpage', {
     //opciones basicas libreria fullpage
 
@@ -15,6 +16,8 @@ const pageSection = new fullpage('#fullpage', {
     navigationToolTips: ['perfil', 'acerca', 'estudios', 'habilidades', 'proyectos', 'contacto'],
     showActiveToolTip: false,
 });
+
+//tipado perfil
 const tipeado = new Typed('.auto__type', {
     strings: ['Desarrollador', 'Autodidacta', 'Ingeniero Mecatrónico'],
     typeSpeed: 100,
@@ -22,6 +25,39 @@ const tipeado = new Typed('.auto__type', {
     loop: true,
 })
 
+//proyectos carrucel
+
+const carruselContainer = document.getElementById('carrusel')
+const cards = Array.from(document.getElementsByClassName('card'))
+const btnSiguiente = document.getElementById('next')
+const btnAnterior = document.getElementById('prev')
+let totalCards = cards.length-1
+let inicio = 0
+console.log(totalCards)
+function carrusel(i = 0){
+    carruselContainer.style.transform = `translateX(${-i*440}px)`;
+    cards[i].setAttribute()
+}
+cards.forEach((card,i) => {
+    card.addEventListener('click', () =>{
+        id=i
+        carrusel(i)
+    })
+})
+btnAnterior.addEventListener('click',()=>{
+    if(inicio > 0){
+        inicio--
+        carrusel(inicio)
+    }
+})
+btnSiguiente.addEventListener('click',()=>{
+    if(inicio < totalCards){
+        inicio++
+        carrusel(inicio)
+    }
+})
+
+//formulario
 function validarInputs(input) {
     switch (input.target.name) {
         case 'name':
@@ -33,33 +69,33 @@ function validarInputs(input) {
                 console.log(document.getElementsByClassName('form__label')[0])
             }
             break;
-            case 'email':
-                if (input.target.value.length > 0) {
-                    document.getElementsByClassName('form__label')[1].classList.add('oculto')
-                    console.log(document.getElementsByClassName('form__label')[1])
-                } else {
-                    document.getElementsByClassName('form__label')[1].classList.remove('oculto')
-                    console.log(document.getElementsByClassName('form__label')[1])
-                }
-                break;
-            case 'subject':
-                if (input.target.value.length > 0) {
-                    document.getElementsByClassName('form__label')[2].classList.add('oculto')
-                    console.log(document.getElementsByClassName('form__label')[2])
-                } else {
-                    document.getElementsByClassName('form__label')[2].classList.remove('oculto')
-                    console.log(document.getElementsByClassName('form__label')[2])
-                }
-                break;
-            case 'text__area':
-                if (input.target.value.length > 0) {
-                    document.getElementsByClassName('form__label')[3].classList.add('oculto')
-                    console.log(document.getElementsByClassName('form__label')[3])
-                } else {
-                    document.getElementsByClassName('form__label')[3].classList.remove('oculto')
-                    console.log(document.getElementsByClassName('form__label')[3])
-                }
-                break;
+        case 'email':
+            if (input.target.value.length > 0) {
+                document.getElementsByClassName('form__label')[1].classList.add('oculto')
+                console.log(document.getElementsByClassName('form__label')[1])
+            } else {
+                document.getElementsByClassName('form__label')[1].classList.remove('oculto')
+                console.log(document.getElementsByClassName('form__label')[1])
+            }
+            break;
+        case 'subject':
+            if (input.target.value.length > 0) {
+                document.getElementsByClassName('form__label')[2].classList.add('oculto')
+                console.log(document.getElementsByClassName('form__label')[2])
+            } else {
+                document.getElementsByClassName('form__label')[2].classList.remove('oculto')
+                console.log(document.getElementsByClassName('form__label')[2])
+            }
+            break;
+        case 'text__area':
+            if (input.target.value.length > 0) {
+                document.getElementsByClassName('form__label')[3].classList.add('oculto')
+                console.log(document.getElementsByClassName('form__label')[3])
+            } else {
+                document.getElementsByClassName('form__label')[3].classList.remove('oculto')
+                console.log(document.getElementsByClassName('form__label')[3])
+            }
+            break;
     }
 }
 const expresionesRegulares = {
