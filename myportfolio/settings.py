b983199dcf39bae60682d86f938d280fb4031b4c
Julian Casallas <julian.casallasb@gmail.com>
2022-11-28 00:33:19 -0300

    configuracion correo de contacto

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -119,10 +119,17 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICFILES_DIRS =[
-    BASE_DIR / "static",
-    'var/www/static'
+    BASE_DIR / 'static/',
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'juliandevcasallas@gmail.com'
+EMAIL_HOST_PASSWORD = 'alrqeuinbhsvpxkl'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
